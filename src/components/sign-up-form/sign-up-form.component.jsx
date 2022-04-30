c79de125d368d03c9523ab19616258e0d733cf85
ctoxy <cedric.bouve@gmail.com>
2022-04-30 14:37:48 +0200

    signup form user with email and password work and got firestore

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,5 @@
 import { Fragment, useState } from 'react';
+import { createAuthUserWithEmailAndPassword,createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
 const defaultFormFields = {
     displayName: '',
@@ -10,7 +11,37 @@ const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
     console.log('saisie formulaire inscription',formFields);
+    //reset formFields
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    }
+    //soumission du formulaire
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+    
+        if (password !== confirmPassword) {
+          alert('passwords do not match');
+          return;
+        }
+    
+        try {
+          const { user } = await createAuthUserWithEmailAndPassword(
+            email,
+            password
+          );
+    
+          await createUserDocumentFromAuth(user, { displayName });
+          resetFormFields();
+        } catch (error) {
+          if (error.code === 'auth/email-already-in-use') {
+            alert('Cannot create user, email already in use');
+          } else {
+            console.log('user creation encountered an error', error);
+          }
+        }
+      };
 
+    // recupération des informations saisis dans le formulaire
     const handleChange = (event) => {
         const {name, value} = event.target;
         setFormFields({...formFields, [name]: value});
@@ -19,7 +50,7 @@ const SignUpForm = () => {
     return(
         <Fragment>
             <h1>Sign up with Email and Password</h1>
-            <form onSubmit={()=> {}}>
+            <form onSubmit={handleSubmit}>
                 <label >Display Name</label>
                 <input 
                     type="text"
--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -1,67 +1,75 @@
-import {initializeApp,} from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import {
-    getAuth,
-    signInWithRedirect,
-    signInWithPopup,
-    GoogleAuthProvider,
-} from 'firebase/auth';
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 
-import {
-    getFirestore,
-    doc,
-    getDoc,
-    setDoc
-
-} from 'firebase/firestore';
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBPV2fjSLQlEZJj02vBXnumy_9Q8a701O4",
-    authDomain: "ecomced-clothing.firebaseapp.com",
-    projectId: "ecomced-clothing",
-    storageBucket: "ecomced-clothing.appspot.com",
-    messagingSenderId: "723627522811",
-    appId: "1:723627522811:web:e64760304711d35e79ce5e"
-  };
-  
-  // Initialize Firebase
-  const firebaseApp = initializeApp(firebaseConfig);
-  const googleProvider = new GoogleAuthProvider();
-  googleProvider.setCustomParameters({
-      prompt: "select_account"
-  });
+  apiKey: "AIzaSyBPV2fjSLQlEZJj02vBXnumy_9Q8a701O4",
+  authDomain: "ecomced-clothing.firebaseapp.com",
+  projectId: "ecomced-clothing",
+  storageBucket: "ecomced-clothing.appspot.com",
+  messagingSenderId: "723627522811",
+  appId: "1:723627522811:web:e64760304711d35e79ce5e",
+};
 
-  export const auth = getAuth();
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
 
-  export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
-  export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
+export const auth = getAuth();
 
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
-  export const db = getFirestore();
+export const db = getFirestore();
 
-  export const createUserDocumentFromAuth = async (userAuth) => {
-      const userDocRef = doc(db, 'users', userAuth.uid);
-      //console.log(userDocRef);
-      //persist la donnée dans firestore
-      const userSnapshot = await getDoc(userDocRef);
-      //console.log(userSnapshot);
-      //verifie si la donnée existe déja
-      //console.log(userSnapshot.exists());
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+  const userDocRef = doc(db, "users", userAuth.uid);
+  //console.log(userDocRef);
+  //persist la donnée dans firestore
+  const userSnapshot = await getDoc(userDocRef);
+  //console.log(userSnapshot);
+  //verifie si la donnée existe déja
+  //console.log(userSnapshot.exists());
 
-      //if user does not exist
-      if (!userSnapshot.exists()) {
-          const  {displayName, email} = userAuth;
-          const createdAt = new Date();
+  //if user does not exist
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
 
-          try {
-              await setDoc(userDocRef, {
-                  displayName,
-                  email,
-                  createdAt
-              })
-          } catch (error) {
-              console.log('error creating the user', error.message);
-          }
-      }
-
-      return userDocRef;
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
   }
+
+  return userDocRef;
+};
+
+//authentification via email and password
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
